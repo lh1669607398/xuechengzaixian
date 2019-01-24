3e29f585606955472b77fc8b21809aac2e23d642
git <1669607398@qq.com>
2019-01-24 21:21:29 +0800

    学成在线-->springboot整合RabbitMQ消息中间件

---
+++ test-rabbitmq-consumer/src/test/java/com/xuecheng/test/rabbitmq/ReceiveHandler.java
@@ -0,0 +1,23 @@
+package com.xuecheng.test.rabbitmq;
+
+import com.xuecheng.test.rabbitmq.config.RabbitmqConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import org.springframework.amqp.core.Message;
+import com.rabbitmq.client.Channel;
+
+@Component
+public class ReceiveHandler {
+    //监听email队列
+    @RabbitListener(queues = {RabbitmqConfig.QUEUE_INFORM_EMAIL})
+    public void receive_email(String msg, Message message, Channel channel) {
+        System.out.println(msg);
+    }
+
+    //监听sms队列
+    @RabbitListener(queues = {RabbitmqConfig.QUEUE_INFORM_SMS})
+    public void receive_sms(String msg, Message message, Channel channel) {
+        System.out.println(msg);
+    }
+
+}
